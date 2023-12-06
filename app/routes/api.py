6529c0422e43c695ec6fb43d3ea5f27b6205d399
Ji-yoh <geovannih514@gmail.com>
2023-12-06 12:48:32 -0800

    set up user logins and sessions

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, request, jsonify
+import sys
+from flask import Blueprint, request, jsonify, session
 from app.models import User
 from app.db import get_db
 
@@ -9,15 +10,53 @@ def signup():
     data = request.get_json()
     db = get_db()
 
-    # create a new user, bracket notation for Python dictionary is the same as dot notation in JS
-    newUser = User(
-        username = data['username'],
-        email = data['email'],
-        password = data['password']
-    )
+    try: # attempt to create a new user w/ try..except
+        # create a new user, bracket notation for Python dictionary is the same as dot notation in JS
+        newUser = User(
+            username = data['username'],
+            email = data['email'],
+            password = data['password']
+        )
 
-    # save in database
-    db.add(newUser)
-    db.commit()
+        # save in database
+        db.add(newUser)
+        db.commit()
+    except:
+        # insert failed, send error to frontend
+        print(sys.exe_info()[0])
+        db.rollback() # rollback on error
+        return jsonify(message = 'Signup failed'), 500
+
+    session.clear()
+    session['user_id'] = newUser.id
+    session['loggedIn'] = True
 
     return jsonify(id = newUser.id)
+
+@bp.route('/users/logout', methods=['POST'])
+def logout():
+    # remove session variables
+    session.clear()
+    return '', 204
+
+@bp.route('/users/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    db = get_db()
+
+    # get user from database
+    try:
+        user = db.query(User).filter(User.email == data['email']).one()
+    except:
+        print(sys.exc_info()[0])
+
+        return jsonify(message = 'Incorrect credentials'), 400
+    
+    if user.verify_password(data['password']) == False:
+        return jsonify(message = 'Incorrect credentials'), 400
+    
+    session.clear()
+    session['user_id'] = user.id
+    session['loggedIn'] = True
+    
+    return jsonify(id = user.id)
